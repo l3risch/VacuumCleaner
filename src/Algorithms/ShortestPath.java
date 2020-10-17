00adbaad1b276fef019880e63bca3fa3d9ea3138
Christian Brzeski <Christian.Brzeski@studium.uni-hamburg.de>
2020-10-17 14:44:00 +0200

    Zwischenstand

--- src/Algorithms/ShortestPath.java
+++ src/Algorithms/ShortestPath.java
@@ -169,7 +169,7 @@ public class ShortestPath extends Basic{
 		graph = calculateShortestPathFromSource(graph, source);
 		
 		List<Node> shortestPath = dest.getShortestPath();
-		
+		shortestPath.add(new Node(_nn.getRow(), _nn.getCol()));
 		System.out.println("Old Path");
 		for(Node node : shortestPath)
 		{
@@ -260,68 +260,91 @@ public class ShortestPath extends Basic{
 	 */
 	private static List<Node> adjustDijkstraPath(List<Node> nodeList)
 	{
+		List<Node> adjustedPath = new LinkedList<Node>();
+		adjustedPath.add(nodeList.get(0));
 		
-		for(int i = 1; i < nodeList.size(); i++)
+		for(int i = 1; i < nodeList.size()-1; i++)
 		{
-			Node currentNode = nodeList.get(i-1);
-			Node nextNode = nodeList.get(i);
+			Node previousNode = nodeList.get(i-1);
+			Node currentNode = nodeList.get(i);
+			Node nextNode = nodeList.get(i+1);
 			
+			int[] lastMove = {currentNode.x - previousNode.x, currentNode.y - previousNode.y};
+			int[] nextMove = {nextNode.x - currentNode.x, nextNode.y - currentNode.y};
 			Coordinates2D[][] nextRobotPosition = Robot.getCoordinates(nextNode.x, nextNode.y);
-
-//			System.out.println("Current: "+ currentNode.x + "; " + currentNode.y);
-//			System.out.println("Next: " + nextNode.x + ", " +nextNode.y);
-
-//			if(isBottomHittingObstacle(nextRobotPosition))
-//			{
-//				if(isRightHittingObstacle(nextRobotPosition))
-//				{
-//					nodeList.set(i-1, new Node(currentNode.x, currentNode.y-3));
-//				}
-//			}
-			
-			modifyNextPosition(nextRobotPosition, currentNode, nextNode, nodeList, i);
-			
-			//Set last element
-			if(i == nodeList.size()-1)
+			if(isHittingObstacle(nextRobotPosition))
 			{
-				modifyNextPosition(nextRobotPosition, currentNode, nextNode, nodeList, i);
+				Node adjNode = new Node(currentNode.x  + lastMove[0], currentNode.y + lastMove[1]);
+				for(int j = i+1; j < nodeList.size(); j++)
+				{
+					nodeList.set(j, new Node(nodeList.get(j).x + lastMove[0], nodeList.get(j).y + lastMove[1]));
+				}
+				if(adjustedPath.get(adjustedPath.size()-1).x != currentNode.x || adjustedPath.get(adjustedPath.size()-1).y != currentNode.y)
+					adjustedPath.add(currentNode);
+					
+				adjustedPath.add(adjNode);
+				nodeList.add(i+1, adjNode);
+				
+			} else {
+				if(adjustedPath.get(adjustedPath.size()-1).x != currentNode.x || adjustedPath.get(adjustedPath.size()-1).y != currentNode.y)
+					adjustedPath.add(currentNode);
 			}
+			
 		}
+
+		
+		adjustedPath.add(nodeList.get(nodeList.size()-1));
 
 			
 		System.out.println("Adj Path: ");
-		for(Node node : nodeList)
+		for(Node node : adjustedPath)
 		{
 			System.out.println(node.x + ", " + node.y);
 		}
 		System.out.println("Nearest Neighbour: " + _nn.getRow() + ", " + _nn.getCol());
 		System.out.println("_________________________");
-		return nodeList;
+		return adjustedPath;
 	}
 	
 
-
-
-	private static void modifyNextPosition(Coordinates2D[][] nextRobotPosition, Node currentNode, Node nextNode, List<Node> nodeList, int i) 
+	private static boolean isHittingObstacle(Coordinates2D[][] nextRobotPosition) 
 	{
-		if(isTopHittingObstacle(nextRobotPosition))
-		{
-			nodeList.set(i-1, new Node(currentNode.x+3, currentNode.y));
 
-		} else if(isRightHittingObstacle(nextRobotPosition))
+		for(int row = 0; row < 4; row++)
 		{
-			nodeList.set(i-1, new Node(currentNode.x, currentNode.y-3));
-		}		
+			for(int col = 0; col < 4; col++)
+			{
+				int robotCellRow = nextRobotPosition[row][col].getRow();
+				int robotCellCol = nextRobotPosition[row][col].getCol();	
+				if(robotCellRow < 0 || robotCellRow >= 64 || robotCellCol < 0 || robotCellCol >= 64)
+				{
+					return true;
+				}
+				if(Table.getMarkedObstacles(robotCellRow, robotCellCol))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
-
+	private static boolean turnInPath(int[] lastMove, int[]nextMove)
+	{
+		if(lastMove[0] == nextMove[0] && lastMove[1] == nextMove[1])
+		{
+			return false;
+		} else {
+			return true;
+		}
+	}
 
 	private static boolean isTopHittingObstacle(Coordinates2D[][] nextRobotPosition) 
 	{
 
 		for(int row = 0; row < 3; row++)
 		{
-			for(int col = 0; col < 4; col++)
+			for(int col = 0; col < 3; col++)
 			{
 				int robotCellRow = nextRobotPosition[row][col].getRow();
 				int robotCellCol = nextRobotPosition[row][col].getCol();	
@@ -334,10 +357,11 @@ public class ShortestPath extends Basic{
 		return false;
 	}
 	
+	
 	private static boolean isRightHittingObstacle(Coordinates2D[][] nextRobotPosition) 
 	{
 
-		for(int row = 0; row < 4; row++)
+		for(int row = 1; row < 4; row++)
 		{
 			for(int col = 1; col < 4; col++)
 			{
@@ -351,6 +375,7 @@ public class ShortestPath extends Basic{
 		}
 		return false;
 	}
+
 
 }
 
